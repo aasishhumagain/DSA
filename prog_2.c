ae28d203511171494b72fb03ec425f02b151f259
aasishhumagain <aasishhumagain1111@gmail.com>
2026-02-10 17:51:14 +0545

    Changed

--- prog_2.c
+++ prog_2.c
@@ -1,39 +1,30 @@
 #include<stdio.h>
 #include<ctype.h>
 #define MAX 100
-
 char stack[MAX];
 int top = -1;
-
 void push(char c) {
     if (top < MAX - 1) {
         stack[++top] = c;
     }
 }
-
 char pop() {
     return stack[top--];
 }
-
 char peek() {
     return stack[top];
 }
-
 int isEmpty() {
     return top == -1;
 }
-
 int precedence(char op) {
     if (op == '+' || op == '-') return 1;
     if (op == '*' || op == '/') return 2;
     return 0;
-
 }
-
 void infixToPostfix(char* infix, char* postfix) {
     int i = 0, j = 0;
     char c;
-
     while ((c = infix[i++]) != '\0') {
         if (isalnum(c)) {
             postfix[j++] = c;
@@ -51,13 +42,11 @@ void infixToPostfix(char* infix, char* postfix) {
             push(c);
         }
     }
-
     while (!isEmpty()) {
         postfix[j++] = pop();
     }
     postfix[j] = '\0';
 }
-
 int evaluatePostfix(char* postfix) {
     int stack[MAX];
     int top = -1;
@@ -80,18 +69,13 @@ int evaluatePostfix(char* postfix) {
     }
     return stack[top];
 }
-
 int main() {
     char infix[MAX], postfix[MAX];
-
     printf("Enter an infix expression: ");
     scanf("%s", infix);
-
     infixToPostfix(infix, postfix);
     printf("Postfix expression: %s\n", postfix);
-
     int result = evaluatePostfix(postfix);
     printf("Evaluation result: %d\n", result);
-
     return 0;
 }
