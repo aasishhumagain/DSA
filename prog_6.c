b4731a314abbcd8917aadbd2f66773b67d39de32
aasishhumagain <aasishhumagain1111@gmail.com>
2026-02-10 17:57:13 +0545

    Changes

--- prog_6.c
+++ prog_6.c
@@ -1,61 +1,48 @@
 #include <stdio.h>
-
 void swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
-
 void minHeapify(int arr[], int n, int i) {
     int smallest = i;
     int left = 2*i + 1;
     int right = 2*i + 2;
-
     if (left < n && arr[left] < arr[smallest])
         smallest = left;
-
     if (right < n && arr[right] < arr[smallest])
         smallest = right;
-
     if (smallest != i) {
         swap(&arr[i], &arr[smallest]);
         minHeapify(arr, n, smallest);
     }
 }
-
 void maxHeapify(int arr[], int n, int i) {
     int largest = i;
     int left = 2*i + 1;
     int right = 2*i + 2;
-
     if (left < n && arr[left] > arr[largest])
         largest = left;
-
     if (right < n && arr[right] > arr[largest])
         largest = right;
-
     if (largest != i) {
         swap(&arr[i], &arr[largest]);
         maxHeapify(arr, n, largest);
     }
 }
-
 void buildMinHeap(int arr[], int n) {
     for (int i = (n/2) - 1; i >= 0; i--)
         minHeapify(arr, n, i);
 }
-
 void buildMaxHeap(int arr[], int n) {
     for (int i = (n/2) - 1; i >= 0; i--)
         maxHeapify(arr, n, i);
 }
-
 void printHeap(int arr[], int n) {
     for (int i = 0; i < n; i++)
         printf("%d ", arr[i]);
     printf("\n");
 }
-
 int main() {
     int n;
     printf("Enter the number of elements:\n");
